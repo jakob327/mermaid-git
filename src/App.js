2dc718dec78c9db0b3c0597734cd8f979d231042
Jakob Hirscheider <jakob.hirscheider@byteleaf.de>
2022-05-07 18:42:31 +0200

    add mermaid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import mermaid from 'mermaid';
+
+const tdGraph = `
+  graph TD
+    A-->B
+    B-->C
+    B-->D
+`
+
+const commits = `
+  gitGraph
+    commit id: "C1"
+    commit id: "C2"
+    commit id: "C3"
+    commit id: "C4"
+`
+
+const branch = `
+  gitGraph
+    commit id: "C1"
+    commit id: "C2"
+    branch feature-branch
+    commit id: "F1"
+    commit id: "F2"
+    checkout main
+    commit id: "C3"
+    commit id: "C4"
+    merge feature-branch
+
+`
+
 
 function App() {
+  useEffect(() => {
+    mermaid.initialize({ startOnLoad: true, theme: 'base' })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mermaid">{commits}</div>
+      <div className="mermaid">{branch}</div>
+    </>
   );
 }
 
